51241f9d2064df0b54ab39f11c31b1ebe50506c0
reecen9696 <snimorak@gmail.com>
2024-10-14 12:11:50 +1100

    changes

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div className="fixed w-full p-4 flex justify-between">
+      <div className="w-16 h-16 rounded-full bg-gray-600 flex items-center justify-center">
+        =
+      </div>
+      <div className="w-16 h-16 rounded-full bg-gray-600 flex items-center justify-center">
+        +
+      </div>
+    </div>
+  );
+};
+
+export default navbar;
---
+++ src/app/pages/mainPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const mainPage = () => {
+  return (
+    <div className="p-4 pt-24 flex-col space-y-4">
+      <div className="w-full h-64 bg-slate-400 p-4">
+        <h1 className="font-bold">Title</h1>
+        <p>Text</p>
+      </div>
+      <div className="w-full h-64 bg-slate-400 p-4">
+        <h1 className="font-bold">Title</h1>
+        <p>Text</p>
+      </div>
+      <div className="w-full h-64 bg-slate-400 p-4">
+        <h1 className="font-bold">Title</h1>
+        <p>Text</p>
+      </div>
+    </div>
+  );
+};
+
+export default mainPage;
